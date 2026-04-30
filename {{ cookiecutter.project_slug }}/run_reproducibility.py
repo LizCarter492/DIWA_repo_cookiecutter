3af2b989bb0792c4a97f4823d7eba4f25ea5a954
Elsa Culler <eculler@gmail.com>
2026-04-30 12:41:59 +0300

    Enhance run_reproducibility.py for file type support
    Refactor run_reproducibility.py to support multiple file types and improve subprocess handling.

--- {{ cookiecutter.project_slug }}/run_reproducibility.py
+++ {{ cookiecutter.project_slug }}/run_reproducibility.py
@@ -1,16 +1,38 @@
-#!/srv/conda/envs/notebook/bin/python
-
 import subprocess
-from path import Path
+from pathlib import Path
 
 # Get all standard element files and put them in order
-se_list = sorted(Path("notebooks").glob("SE*.ipynb"))
+se_list = Path("notebooks").glob("SE*")
 
-# Run each 
-for se_path in se_list:
+# Run each standard element in order
+### ------------------------------------------------------ ###
+###          If you have other types of files              ###
+### you will need to add code and dependencies to run them ###
+### ------------------------------------------------------ ###
+
+for se_path in sorted(se_list):
     print(f"Running {se_path}")
-    subprocess.run(
-        f"jupyter nbconvert --execute --to notebook --inplace {se_path}", 
-        shell=True, check=True)
+    try:
+        if se_path.suffix == ".ipynb":
+            subprocess.run(
+                ["jupyter", "nbconvert", "--execute",
+                 "--to", "notebook", "--inplace", str(se_path)],
+                check=True, capture_output=True, text=True
+            )
+            
+        elif se_path.suffix == ".Rmd":
+            subprocess.run(
+                ["Rscript", "-e", f'library(knitr);knit("{se_path}")'],
+                check=True, capture_output=True, text=True
+            )
+            
+        else:
+            print("File type not supported...skipping.")
+            
+    except subprocess.CalledProcessError as e:
+        print(f"Error running {se_path}:")
+        print(e.stdout)
+        print(e.stderr)
+        raise
 
 print("Workflow complete")
