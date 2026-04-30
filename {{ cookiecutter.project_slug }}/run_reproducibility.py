20e89d84f4bf924565b2d1fcd7aca0b8bbc9c33c
Elsa Culler <eculler@gmail.com>
2026-04-30 11:52:02 +0300

    Refactor notebook execution to use glob for discovery
    Replaced hardcoded notebook list with glob to dynamically find notebooks.

--- {{ cookiecutter.project_slug }}/run_reproducibility.py
+++ {{ cookiecutter.project_slug }}/run_reproducibility.py
@@ -1,4 +1,12 @@
 import subprocess
-for nb in ["SE1_data_access.ipynb","SE2_data_processing.ipynb","SE3_model_train_validate.ipynb","SE4_generate_outputs.ipynb"]:
-    subprocess.run(f"jupyter nbconvert --execute --to notebook --inplace notebooks/{nb}", shell=True, check=True)
+from glob import glob
+
+nb_list = sorted(glob("SE*.ipynb"))
+
+for nb in nb_list:
+    print("Running nb")
+    subprocess.run(
+        f"jupyter nbconvert --execute --to notebook --inplace notebooks/{nb}", 
+        shell=True, check=True)
+
 print("Workflow complete")
